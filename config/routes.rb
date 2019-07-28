31280d9dd00b99773df7552ea6220f3ed346ad0f
芦原亨 <dennkidaisuki1007@gmail.com>
2019-07-28 22:44:43 +0900

    INDEX画面制作

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/items' => 'items#index'
-  post '/items' => 'items#create'
-  get '/items/new' => 'items#new'   # newとshowの順番に気をつけて下さい
-  get '/items/:id' => 'items#show'  # 逆に書いてしまうと、全てshowで処理されます
-  get '/items/:id/edit' => 'items#edit'
-  patch '/items/:id' => 'items#update'
-  delete '/items/:id' => 'items#destroy'
+
+  resources :items
+
 end
